45daffb14d30c295248c4edd68424c58c431af79
lukef3 <lukefoley@outlook.com>
2023-10-25 13:41:54 +0100

    fix

--- app_server/models/tasks.js
+++ app_server/models/tasks.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    taskID: int,
+    taskID: Number,
     name: String,
     category: String,
-    priority: Int,
+    priority: Number,
     dueDate: Date
 });
